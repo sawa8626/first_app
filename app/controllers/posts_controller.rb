1b4618f802c6ba546615844fd13255192d6a59da
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-07-29 19:58:45 +0900

    サーバーサイドの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,25 @@
 class PostsController < ApplicationController
-
+# 一覧ページ
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
-
+# 新規投稿
   def create
     Post.create(content: params[:content])
+    redirect_to action: :nidex
   end
 
+  # 既読のチェック
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
